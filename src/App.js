37e971436ac7749f4786bd4fd30a353dffc8a5e5
Shahar Shemesh <shahar@usa.com>
2023-02-19 21:22:48 +0200

    Changes to be committed: 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import './App.css';
 import {Header} from './components/Header';
 import {AddItem} from './components/AddItem';
 import {Clock} from './components/Clock';
-import {IsToggleOnExample} from './training/IsToggleOnExample';
-import {NameFormExample} from './training/NameFormExample';
-import {HandleSubmitExample} from './training/HandleSubmitExample';
-import {FormAndSelectExample} from './training/FormAndSelectExample';
 import {Categories} from './components/Categories'
 import { Component, useEffect, useState } from 'react';
 import Exception from './components/Exception';
